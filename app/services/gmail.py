26e890509eff37cf95aba63d80ba5b49c4d244b1
naveen <naveensheejachacko@gmail.com>
2026-09-10 20:28:23 +0530

    Update .gitignore to exclude client_secret files and add Google Sheets API scope in gmail.py

--- app/services/gmail.py
+++ app/services/gmail.py
@@ -28,6 +28,7 @@ logger = logging.getLogger(__name__)
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/spreadsheets",
 ]
 
 
